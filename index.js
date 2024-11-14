921d9e81381623164d0b6db0ab9f08816f7c28d1
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2024-11-14 12:29:37 +0000

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class User {
 class Exercise {
     constructor(description, duration, date) {
         this.description = description;
-        this.duration = duration;
+        this.duration = new Number(duration);
         this.date = date ? new Date(date).toDateString() : new Date().toDateString();
     }
 }
